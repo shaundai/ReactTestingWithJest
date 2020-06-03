fc9d6e236f46c331d07ea446623c012de9c1e662
Shaundai Person <shaundaiperson@gmail.com>
2020-06-02 23:59:39 -0400

    more learning - testing

--- src/NewMovie.test.js
+++ src/NewMovie.test.js
@@ -3,10 +3,12 @@ import { render, cleanup, fireEvent } from '@testing-library/react';
 import NewMovie from './NewMovie';
 
 test('<NewMovie>', () => {
-    const { debug, getByTestId, queryByTestId } = render(<NewMovie />);
+    const { debug, getByTestId, queryByTestId, container } = render(<NewMovie />);
     debug();
 
     expect(getByTestId('page-title').textContent).toBe('New Movie');
     expect(queryByTestId('movie-form').textContent).toBeTruthy;
+
+    console.log(container)
 
 })
